64be8871823829af30d25e1f7aa25a764833f74d
Ruby1083 <chaupuiying_3@hotmail.com>
2025-06-17 16:33:04 -0700

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.title("Staff Apparel Order Form")
 inventory = [
     {
         "Item": "T-shirt",
-        "Image": "https://i.imgur.com/wQLUiUH.jpeg",
+        "Image": "https://i.imgur.com/2iQt8qs.jpg",
         "Sizes": ["XS", "S", "M", "L", "XL", "2XL", "3XL"]
     },
     {
